10f49a00763f2d37aa09fea45f153a12950e5dd3
Nick Geiger <nick@nickgeiger.com>
2025-02-11 13:29:41 -0800

    updated server archive to move all maps to a timestamped archive dir for later pull/purge

--- app/utils/archive-course-maps.sh
+++ app/utils/archive-course-maps.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Destination = Input
-# Check if the destination directory argument is provided
-if [ -z "$1" ]; then
-  echo -e "Usage: ./archive-course-maps.sh [output_dir]\n - output_dir is missing" >&2  # Output error to stderr
-  exit 1  # Exit with a non-zero status to indicate an error
-fi
-dest="$1"  # Assign the first argument to the destination variable
-# Add trailing slash to destination if it's missing
-if [[ ! "$dest" =~ /$ ]]; then  # Use regex to check for trailing slash. =/ is regex match
-  dest+="/"
-fi
-
 # Source = Published Maps Dir
 #source="zsource/" # TEST
 source="/home/nick/apps/nickgeiger/api/discy/publish-course-map/published-maps/" # Make sure the trailing slash is included
+dest="/home/nick/discy-published-map-archives/archive-$(date +%s)/"
 
 echo "Archiving: $source"
 echo "Destination: $dest"
