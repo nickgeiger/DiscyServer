8afabb965f34d4d7b86ba8b08c43a65bd87685b8
Nick Geiger <nick@nickgeiger.com>
2025-02-11 13:47:51 -0800

    change archive to simply pull archives and purge from server

--- archive/archive.sh
+++ archive/archive.sh
@@ -2,20 +2,17 @@
 ## accidentally continue and nuke server archive if it wasn't backed up
 set -e
 
-
-archive_dir="discy-published-map-archives/$(uuidgen)"
-##echo "ARCHIVE: $archive_dir"
-
-echo "ssh nick@nickgeiger.com \"/home/nick/apps/nickgeiger/api/discy/utils/archive-course-maps.sh $archive_dir\""
-ssh nick@nickgeiger.com "/home/nick/apps/nickgeiger/api/discy/utils/archive-course-maps.sh $archive_dir"
-
-server_archive_dir="/home/nick/$archive_dir"
+server_archive_dir="/home/nick/discy-published-map-archives/"
 local_archive_dir="archives/"
 
-mkdir -p $local_archive_dir
-echo "rsync -rv --update nick@nickgeiger.com:$server_archive_dir $local_archive_dir"
-rsync -rv --update nick@nickgeiger.com:$server_archive_dir $local_archive_dir
+echo "Archiving: $server_archive_dir"
+echo "Destination: $local_archive_dir"
 
-echo "ssh nick@nickgeiger.com \"rm -rf $server_archive_dir\""
-ssh nick@nickgeiger.com "rm -rf $server_archive_dir"
+mkdir -p $local_archive_dir
+
+echo "rsync -rv --update nick@nickgeiger.com:${server_archive_dir}archive-* $local_archive_dir"
+rsync -rv --update nick@nickgeiger.com:${server_archive_dir}archive-* $local_archive_dir
+
+echo "ssh nick@nickgeiger.com \"rm -rf ${server_archive_dir}archive-*\""
+echo "ssh nick@nickgeiger.com \"rm -rf ${server_archive_dir}archive-*\""
 
